6dbc8e73c5df5cc70ff5c920824f623b6c39abcf
om <cuomchoudhary16@gmail.com>
2024-03-05 18:53:50 +0530

    first commit

---
+++ DemoWebAPI/Book.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+
+namespace DemoWebAPI
+{
+    public class Book
+    {
+        public ObjectId Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+
+}
---
+++ DemoWebAPI/Controllers/BooksController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace DemoWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly MongoDbContext _context;
+
+        public BooksController(MongoDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> Get()
+        {
+            var collection = _context.GetCollection<Book>("books");
+            var books = collection.Find(book => true).ToList();
+            return books;
+        }
+
+        [HttpPost]
+        public ActionResult<Book> Post(Book book)
+        {
+            var collection = _context.GetCollection<Book>("books");
+            collection.InsertOne(book);
+            return book;
+        }
+
+        // Implement other CRUD operations (PUT, DELETE) as needed
+    }
+
+}
